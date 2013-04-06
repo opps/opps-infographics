951611a6ac1a191aad01b47c7cbdba4b8ac46f01
Bruno Rocha <rochacbruno@gmail.com>
2013-04-06 21:07:05 +0000

    Fix PEP8

--- setup.py
+++ setup.py
@@ -24,22 +24,24 @@ try:
 except:
     long_description = infographics.__description__
 
-setup(name='opps-infographics',
-        namespace_packages=['opps', 'opps.infographics'],
-        version=infographics.__version__,
-        description=infographics.__description__,
-        long_description=long_description,
-        classifiers=classifiers,
-        keywords='infographic opps cms django apps magazines websites',
-        author=infographics.__author__,
-        author_email=infographics.__email__,
-        url='http://oppsproject.org',
-        download_url="https://github.com/YACOWS/opps-infographics/tarball/master",
-        license=infographics.__license__,
-        packages=find_packages(exclude=('doc', 'docs',)),
-        package_dir={'opps': 'opps'},
-        install_requires=install_requires,
-        include_package_data=True,
-        package_data={
-           'infographics': ['templates/*']
-        })
+setup(
+    name='opps-infographics',
+    namespace_packages=['opps', 'opps.infographics'],
+    version=infographics.__version__,
+    description=infographics.__description__,
+    long_description=long_description,
+    classifiers=classifiers,
+    keywords='infographic opps cms django apps magazines websites',
+    author=infographics.__author__,
+    author_email=infographics.__email__,
+    url='http://oppsproject.org',
+    download_url="https://github.com/YACOWS/opps-infographics/tarball/master",
+    license=infographics.__license__,
+    packages=find_packages(exclude=('doc', 'docs',)),
+    package_dir={'opps': 'opps'},
+    install_requires=install_requires,
+    include_package_data=True,
+    package_data={
+        'infographics': ['templates/*']
+    }
+)
